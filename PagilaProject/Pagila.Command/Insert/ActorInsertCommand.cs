13ababdfd06f87a3857eacc6efc7fb47b3bc2dc8
Mustafa.SACLI <Mustafa.SACLI@GSBBIDB330>
2021-05-05 02:19:37 +0300

    projects added.

---
+++ PagilaProject/Pagila.Command/Insert/ActorInsertCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace Pagila.Command.Insert
+{
+    public class ActorInsertCommand
+    {
+        public string FirstName
+        { get; set; }
+
+        public string LastName
+        { get; set; }
+    }
+}
