eae21c880c5bb1bee6dbb769420638735e269e57
mustafasacli <mustasacli@hotmail.com>
2023-04-10 17:58:45 +0300

    code, package and refence arranged.

--- PagilaProject/Pagila.QueryHandlers/City/CityReadAllQueryHandler.cs
+++ PagilaProject/Pagila.QueryHandlers/City/CityReadAllQueryHandler.cs
@@ -2,7 +2,10 @@
 using Pagila.Query.City;
 using Pagila.ViewModel;
 using SimpleInfra.Common.Response;
+using Simply.Common;
 using Simply.Crud;
+using Simply.Crud.Enums;
+using Simply.Crud.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -24,18 +27,50 @@ namespace Pagila.QueryHandlers.City
 
             using (var database = GetDatabase())
             {
-                var actorEntList = database.GetAll<CityEntity>();
+                IWhere<CityEntity> city =
+                database.Where<CityEntity>()
+                    .AddAndCondition(q => q.CityId > 0)
+                    .AddPropertyForSelect(q => q.CityId)
+                    .AddPropertyForSelect(q => q.City)
+                    .AddPropertyForSelect(q => q.CountryId)
+                    .AddPropertyForSelect(q => q.LastUpdate)
+                    .AddOrderClause(q => q.LastUpdate, isDescending: true)
+                    .AddJoin(
+                    database.BuildJoin<CityEntity, CountryEntity>(JoinTypes.LeftJoin)
+                    .AddJoin(p => p.CountryId, q => q.CountryId),
+                    database.Where<CountryEntity>()
+                    .AddAndCondition(q => q.CountryId > 0)
+                    .AddPropertyForSelect(q => q.Country, "CountryName")
+                    );
+
+                var liste = database.PartialSelect(city).ToList();
                 response.Data = new CityList
                 {
-                    Cities = actorEntList.Select(p => new CityViewModel
+                    Cities = liste.Select(p => new CityViewModel
                     {
                         CityId = p.CityId,
                         City = p.City,
                         CountryId = p.CountryId,
-                        LastUpdate = p.LastUpdate
-                    }).OrderByDescending(p => p.LastUpdate)
-                    .ToList() ?? new List<CityViewModel>()
+                        LastUpdate = p.LastUpdate,
+                        CountryName = p.CountryName
+                    }).ToList() ?? new List<CityViewModel>()
                 };
+                //var entityList = database.GetAll<CityEntity>();
+                //var countryIdList = entityList.Select(q => q.CountryId).Distinct().ToArray();
+                //var where = database.Where<CountryEntity>().AddAndListContainsCondition(q => q.CountryId, countryIdList);
+                //var countryList = database.Select(where);
+                //response.Data = new CityList
+                //{
+                //    Cities = entityList.Select(p => new CityViewModel
+                //    {
+                //        CityId = p.CityId,
+                //        City = p.City,
+                //        CountryId = p.CountryId,
+                //        LastUpdate = p.LastUpdate,
+                //        CountryName = countryList.FirstOrDefault(q => q.CountryId == p.CountryId)?.Country
+                //    }).OrderByDescending(p => p.LastUpdate)
+                //    .ToList() ?? new List<CityViewModel>()
+                //};
                 response.ResponseCode = response.Data?.Cities?.Count ?? 0;
                 response.RCode = response.ResponseCode.ToString();
             }
